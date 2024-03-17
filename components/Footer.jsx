c90f4896e2329163867d6bd5715de6453826ffc6
mitso-ksr <mitsonary@gmail.com>
2024-03-17 17:40:07 +0330

    components folder added

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      Here goes my footer
+    </div>
+  )
+}
+
+export default Footer
---
+++ components/Layout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom'
+import Footer from './Footer'
+
+export default function Layout () {
+    return (
+        <>
+            <Header />
+            <Outlet />
+            <Footer />
+        </>
+    )
+}
