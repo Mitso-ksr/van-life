9089dc5afdceec4e25fd49fae5033376370f6cc0
mitso-ksr <mitsonary@gmail.com>
2024-03-17 18:30:33 +0330

    outlet componets fix

--- pages/Host/Dashboard.jsx
+++ pages/Host/Dashboard.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 function Dashboard() {
   return (
+    <>
     <h1>
         Dashboard goes here
     </h1>
+    <Outlet />
+    </>
   )
 }
 
